d975b5fccba5c994119b43edc07fa5c19406559e
Maksym Zalietin <maksym.zalietin@gmail.com>
2026-03-24 12:51:48 +0100

    Initial commit

---
+++ src/main/java/me/mzalietin/mdbproject/gateway/MdbGatewayApplication.java
@@ -0,0 +1,13 @@
+package me.mzalietin.mdbproject.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdbGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdbGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mzalietin/mdbproject/gateway/MdbGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mzalietin.mdbproject.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdbGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
